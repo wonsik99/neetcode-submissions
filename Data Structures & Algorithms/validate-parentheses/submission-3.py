d45dc2110805710d113877d07ab6083c8cebb51d
Wonsik Jung <algrthm@umich.edu>
2026-08-16 13:06:27 -0400

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        pairs = {
+            ")":"(",
+            "]":"[",
+            "}":"{"
+        }
+        
+        stack = []
+
+        for c in s:
+            if c in pairs:
+                if stack and stack[-1] == pairs[c]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(c)
+
+        return True if not stack else False
